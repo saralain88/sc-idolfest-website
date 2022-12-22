c29e28ca6f1ab92eb34de91b6e38a50072738a42
SarahLynne <saralain88@gmail.com>
2022-12-22 14:40:36 -0500

    Adding vendor link

--- src/pages/index.js
+++ src/pages/index.js
@@ -47,11 +47,19 @@ const IndexPage = () => {
 
         {/* Kinda hacked this in, remove when we have full navigation enabled */}
         <Box style={{marginTop: '32px'}}>
-          <p>Want to perform at SoCal IdolFest? Apply below!</p>
+          <p>Want to be part of SoCal IdolFest? Apply below!</p>
           <Button style={{width: "100%"}} variant="contained" className="cta" href="https://docs.google.com/forms/d/e/1FAIpQLSeKFD9nhgx_1AbRvjYX-CWFi96PFPgFoN2AF-kwz7gJOCzGCw/viewform" target="_blank">
             Apply to Perform
           </Button>
+          <Button style={{width: "100%"}} variant="contained" className="cta" href="https://docs.google.com/forms/d/e/1FAIpQLScY61eFDGhnNvpCb_pLn33rr7sMCcxKCfbJ5F-GGFSfLdidnw/viewform" target="_blank">
+            Apply as a Vendor
+          </Button>
+          {/*
+          <Button style={{width: "100%"}} variant="contained" className="cta" href="" target="_blank">
+            Apply to Volunteer
+          </Button>*/}
         </Box>
+
         {/* End of hacky code */}
 
         <br /><br />
